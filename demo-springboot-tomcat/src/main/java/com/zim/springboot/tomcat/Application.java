7fe8643b1189ae213cebcddf58f5095b7d803630
zhenwei.liu <zhenwei.liu@bianlifeng.com>
2019-11-04 15:59:41 +0800

    add spring boot for tomcat

---
+++ demo-springboot-tomcat/src/main/java/com/zim/springboot/tomcat/Application.java
@@ -0,0 +1,13 @@
+package com.zim.springboot.tomcat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author zhenwei.liu
+ * @since 2019-11-04
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+}
